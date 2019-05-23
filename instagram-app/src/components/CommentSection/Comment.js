86cc39be74dcc54f65d0b81f3b4c96a69faa941c
Tim Dill <brodt7258@gmail.com>
2019-05-22 22:14:13 -0400

    add subcomponensts, implement propTypes

---
+++ instagram-app/src/components/CommentSection/Comment.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Comment = () => {
+  return (
+    <div>
+      Comment
+    </div>
+  );
+};
+
+Comment.propTypes = {
+  id: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+};
+
+export default Comment;
